a488d7ccf8b0c968d2791e63a12391a1a807bf60
ckdtjs505 <ckdtjs505@naver.com>
2021-07-11 14:23:39 +0900

    feat : create

---
+++ static/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true,
+    sectionsColor: ["black", "orange"]
+  });
+
+  //methods
+  //   $.fn.fullpage.setAllowScrolling(true);
+});
