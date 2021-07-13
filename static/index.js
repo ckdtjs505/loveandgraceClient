f32fd46069fc482a64edcc12322aa1499109bec8
ckdtjs505 <ckdtjs505@naver.com>
2021-07-14 08:10:24 +0900

    fix : datae

--- static/index.js
+++ static/index.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jul 30 2021 15:55:39").getTime();
+var countDownDate = new Date("Jul 31 2021 13:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
